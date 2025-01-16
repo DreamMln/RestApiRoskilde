051d5b5c1521ccfc0edc99d6aefa054574a7ca59
DreamMln <maix2374@edu.zealand.dk>
2025-01-16 11:39:17 +0100

    db tors

---
+++ RestApiRoskilde/Managers/Secrets.cs
@@ -0,0 +1,14 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace RestApiRoskilde.Managers
+{
+    public class Secrets
+    {
+        //local DB
+        public static readonly string ConnectionString =
+        //"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RosKommune;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = MyDb; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
+    }
+}
